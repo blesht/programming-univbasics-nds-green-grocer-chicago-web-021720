3828e90d838e34281f421fed1a383ff78f2ff9c7
bryan lesht <bryan.lesht@colorado.edu>
2020-02-11 18:56:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,24 @@
 def find_item_by_name_in_collection(name, collection)
+  row_index = 0 
+  while row_index < collection.length do 
+    if collection[row_index][:item] == name 
+      return collection[row_index]
+    end
+  row_index += 1 
+  
+  end 
+  
   # Implement me first!
   #
   # Consult README for inputs and outputs
 end
 
 def consolidate_cart(cart)
+  
+  
+  pp cart 
+  puts " THIS IS THE CART ***** #{cart}"
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
